85a7c8b0aab141a495cb4ea5205d4fa34dc0adaf
Caroline Neumann <nn.carol@protonmail.com>
2024-02-08 17:25:32 +0100

    add photos section

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-import fs from 'node:fs';
-import os from 'node:os';
-import { parse } from 'csv-parse';
-// Note, the `stream/promises` module is only available
-// starting with Node.js version 16
-import { finished } from 'stream/promises';
+function imageClicked() {
+  console.log('Image was clicked!');
+  // Add your custom logic here
+  this.src = "./src/assets/photos/caixa_anorina.jpg"
+}
 
+const arrayOfImages = document.getElementsByClassName('clickableImage');
 
-// Read and process the CSV file
-const processFile = async () => {
-    const records = [];
-    const parser = fs
-      .createReadStream(`./asset_matching.csv`)
-      .pipe(parse({
-      // CSV options if any
-      }));
-    parser.on('readable', function(){
-      let record; while ((record = parser.read()) !== null) {
-      // Work with each record
-        records.push(record);
-      }
-    });
-    await finished(parser);
-    return records;
-};
-
-const result = await processFile();
-
-console.log(result);
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-  }
-
-const randomNumber = getRandomInt(1,4)
+for (const image of arrayOfImages) {
+  image.addEventListener('click', imageClicked);
+}
