3f37c884f4af8e9557c0b2809b82270b094dc8de
Caroline Neumann <nn.carol@protonmail.com>
2024-02-09 17:20:27 +0100

    add slider

--- index.js
+++ index.js
@@ -9,3 +9,36 @@ const arrayOfImages = document.getElementsByClassName('clickableImage');
 for (const image of arrayOfImages) {
   image.addEventListener('click', imageClicked);
 }
+
+
+//Slider
+document.addEventListener("DOMContentLoaded", function() {
+  var slides = document.querySelectorAll(".slide");
+  var currentSlide = 0;
+
+  // Show initial slide
+  slides[currentSlide].classList.add("active");
+
+  // Function to show a specific slide
+  function showSlide(index) {
+    // Hide all slides
+    slides.forEach(function(slide) {
+      slide.classList.remove("active");
+    });
+    // Show the slide at the given index
+    slides[index].classList.add("active");
+  }
+
+  // Event listener for previous button
+  document.querySelector(".prev").addEventListener("click", function() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  });
+
+  // Event listener for next button
+  document.querySelector(".next").addEventListener("click", function() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  });
+});
+
